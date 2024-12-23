818c31aee9ebab2f1da75a2fada2a898f20426f4
Catalin Modan <contact@cmodan.ro>
2024-12-23 16:44:55 +0200

    feat: add initial scripts

---
+++ sql/V1_2__CreateVegetablesSchema.sql
@@ -0,0 +1,5 @@
+SET search_path TO "garden.db";
+
+CREATE SCHEMA "vegetables";
+GRANT USAGE ON SCHEMA "vegetables" TO ${VegetablesUserName};
+GRANT USAGE ON SCHEMA "vegetables" TO ${VegetablesUserName};
