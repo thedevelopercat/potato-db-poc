f8a5b1a2792cb71590dc7d28bc2a00cbd32ace4b
Catalin Modan <contact@cmodan.ro>
2024-12-27 21:48:37 +0200

    feat: add repeatable scripts

---
+++ sql/repeatable/R__AllowAccessToSequences.sql
@@ -0,0 +1,2 @@
+-- allow select on sequences
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA "application" TO ${VegetablesUserName};
---
+++ sql/repeatable/R__AllowAccessToTables.sql
@@ -0,0 +1,2 @@
+-- grant the user access to the table
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA "application" TO ${VegetablesUserName};
