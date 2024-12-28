91049b3fce6b342f3412bee8d8eb7a5d7599f5b0
Catalin Modan <contact@cmodan.ro>
2024-12-28 22:49:41 +0200

    fix: cleanup script order

--- sql/after/afterClean.sql
+++ sql/after/afterClean.sql
@@ -1,8 +1,8 @@
 -- cleanup leftover objects
 -- users,schemas,etc
 
--- cleanup leftover role
-DROP ROLE IF EXISTS ${VegetablesUserName};
-
 -- cleanup leftover schema
 DROP SCHEMA IF EXISTS "application" CASCADE;
+
+-- cleanup leftover role
+DROP ROLE IF EXISTS ${VegetablesUserName};
